68c180833778946e5834edc4639c4008b6a3800f
Evan Fabiani <fabiani.evan@icloud.com>
2023-03-28 13:44:46 +0200

    index(img trees) new(simple form)

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -1,2 +1,20 @@
 class TreesController < ApplicationController
+  def index
+    @trees = Tree.all
+  end
+
+  def new
+    @tree = Tree.new
+  end
+
+  private
+
+  def set_tree
+    @tree = Tree.find(params[:id])
+  end
+
+  def tree_params
+    params.require(:tree).permit(:name, :address, :fruit_type, :description,
+                                 :price_per_year, :quantity_per_year)
+  end
 end
